aacef74d861b1d7af948f03ee50e8ebb5c235424
eliyya <tecno_eli@hotmail.com>
2023-02-09 20:51:38 -0600

    export any base

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,24 @@
-declare module 'yasha'
+export const api = {
+    Youtube,
+    Soundcloud,
+    Spotify,
+}
+
+export const Source: any
+export const Track: any
+export const TrackPlayer: any
+export const VoiceConnection: any
+/**
+module.exports = {
+	api: {
+		Youtube: require('./src/api/Youtube'),
+		Soundcloud: require('./src/api/Soundcloud'),
+		Spotify: require('./src/api/Spotify')
+	},
+
+	Source: require('./src/Source'),
+	Track: require('./src/Track'),
+	TrackPlayer: require('./src/TrackPlayer'),
+	VoiceConnection: require('./src/VoiceConnection'),
+};
+ */
