81233ca3224fbc0568639ab531368f0b8a93d113
eliyya <tecno_eli@hotmail.com>
2023-02-26 14:12:24 -0600

    regrese export

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,37 @@
-// import Youtube from './types/api/Youtube'
-// import Soundcloud from './types/api/Soundcloud'
-// import Spotify from './types/api/Spotify'
-// import Source from './types/Source'
-// import Track from './types/Track'
-// import TrackPlayer from './types/TrackPlayer'
-// import VoiceConnection from './types/VoiceConnection'
+import Youtube from './types/api/Youtube'
+import Soundcloud from './types/api/Soundcloud'
+import Spotify from './types/api/Spotify'
+import Source from './types/Source'
+import Track from './types/Track'
+import TrackPlayer from './types/TrackPlayer'
+import VoiceConnection from './types/VoiceConnection'
 
-module.exports = {
-    api: {
-        Youtube: require('./src/api/Youtube'),
-        Soundcloud: require('./src/api/Soundcloud'),
-        Spotify: require('./src/api/Spotify'),
-    },
+// module.exports = {
+//     api: {
+//         Youtube: require('./src/api/Youtube'),
+//         Soundcloud: require('./src/api/Soundcloud'),
+//         Spotify: require('./src/api/Spotify'),
+//     },
 
-    Source: require('./src/Source'),
-    Track: require('./src/Track'),
-    TrackPlayer: require('./src/TrackPlayer'),
-    VoiceConnection: require('./src/VoiceConnection'),
+//     Source: require('./src/Source'),
+//     Track: require('./src/Track'),
+//     TrackPlayer: require('./src/TrackPlayer'),
+//     VoiceConnection: require('./src/VoiceConnection'),
+// }
+declare const api: {
+    Youtube: typeof Youtube
+    Soundcloud: typeof Soundcloud
+    Spotify: typeof Spotify
 }
-// declare const api: {
-//     Youtube: typeof Youtube
-//     Soundcloud: typeof Soundcloud
-//     Spotify: typeof Spotify
-// }
 
-// export { Source, Track, TrackPlayer, VoiceConnection, api }
+export { Source, Track, TrackPlayer, VoiceConnection, api }
 
-// declare const yasha: {
-//     Source: typeof Source
-//     Track: typeof Track
-//     TrackPlayer: typeof TrackPlayer
-//     VoiceConnection: typeof VoiceConnection
-//     api: typeof api
-// }
+declare const yasha: {
+    Source: typeof Source
+    Track: typeof Track
+    TrackPlayer: typeof TrackPlayer
+    VoiceConnection: typeof VoiceConnection
+    api: typeof api
+}
 
-// export default yasha
+export default yasha
